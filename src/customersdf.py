a96dca79ad733b49af4bb50447be47ed7403ad5c
Akula, Venu <akulavenu.bigdata@gmail.com>
2024-07-20 12:15:56 +0100

    updated to display rank for all

--- src/customersdf.py
+++ src/customersdf.py
@@ -14,6 +14,6 @@ window_spec = Window.orderBy(col("count").desc())
 
 df2 = df1.withColumn("rank", rank().over(window_spec))
 
-df3 = df2.where(df2.rank == 3)
+# df3 = df2.where(df2.rank == 3)
 
-df3.show()
+df2.show()
