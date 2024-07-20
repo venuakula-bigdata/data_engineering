1c78d0b86d5e24a1a4471d6df8fd086955d4dd8a
Akula, Venu <akulavenu.bigdata@gmail.com>
2024-07-20 12:08:20 +0100

    customers dataframe

--- src/customersdf.py
+++ src/customersdf.py
@@ -1,1 +1,19 @@
-print('')
+from pyspark.sql import SparkSession
+
+from pyspark.sql.functions import col, rank
+
+from pyspark.sql.window import Window
+
+spark = SparkSession.builder.appName("customerdf").getOrCreate()
+
+df = spark.read.option("header", "true").csv("customers.csv")
+
+df1 = df.groupby("country").count()
+
+window_spec = Window.orderBy(col("count").desc())
+
+df2 = df1.withColumn("rank", rank().over(window_spec))
+
+df3 = df2.where(df2.rank == 3)
+
+df3.show()
